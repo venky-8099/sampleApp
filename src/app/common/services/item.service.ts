81bf6d6e4e46f1de1f3b28068c84097318abf867
chandra pothireddy <venky.vr.8099@gmail.com>
2022-10-26 08:54:25 +0530

    Adding subject

--- src/app/common/services/item.service.ts
+++ src/app/common/services/item.service.ts
@@ -1,15 +1,18 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Item } from '../models/item';
-import { Observable } from 'rxjs';
+import { Observable, Subject } from 'rxjs';
 
 @Injectable()
 export class ItemService{
-    constructor(private http:HttpClient){}
+    sub$ = new Subject<any>();
+    constructor(private http:HttpClient){
+        
+    }
     addItem(item:Item){
         return this.http.post("http://localhost:3200/api/addItem",item);
     }
-    getItems():Observable<Item[]>{
-        return this.http.get<Item[]>("http://localhost:3200/api/listitems");
+    getItems(){
+        return this.http.get("http://localhost:3200/api/listitems");
     }
 }
